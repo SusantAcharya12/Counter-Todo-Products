7ed6a4ffcc58b4381ff7e5cf98e793004912c9f3
Susant Acharya <susantacharya704@gmail.com>
2024-02-14 22:14:13 +0545

    Reached product item page need to work on design after fetching api

--- src/Product.js
+++ src/Product.js
@@ -4,31 +4,73 @@ import { useParams } from "react-router-dom"
 
 
 function Product() {
-    const [data,setData] = useState({});
+    const [data,setData] = useState([]);
+    console.log(data)
 
 
     const {id}= useParams();
     
     useEffect(()=>{
         const fetchData = () => {
-            const dataValue = fetch(`https://dummyjson.com/products/${id}`).then(res=> res.json()).then((products) => setData(products));
+            const dataValue = fetch(`https://dummyjson.com/products/${id}`).then(res=> res.json()).then((products) => setData([products]));
 
         }
+
 
         fetchData()
     
 
     },[id])
 
-    console.log(data)
 
   return (
     <div>
-        hello
-        
+
+      {
+        data.map(item=>(
+          <div className="Top-product" key={item.id}>
+            <div className="container">
+
+            <div className="grid-product">
+
+              <div className="parent-grid-image">
+                <div className="child-grid-image">
+                  <img src={item.images[0]}/>
+                  <img className="child-grid-image-2" src={item.images[1]}/>
+
+                </div>
+
+                <div className="single-child-grid">
+                <img className="single-child-img" src={item.images[2]}/>
+
+                </div>
+
+              </div>
+
+              <div className="product-content">
+                <img src={item.images[1]}/>
+                <h1>{item.brand}</h1>
+                <h2>{item.category}</h2>
+                <p> {item.description}</p>
+                <h4>Price: ${item.price}</h4>
+                <p>Rating:<strong>{item.rating}</strong></p>
+
+
+              </div>
+
+            </div>
+
+            </div>
+          </div>
+        ))
       
-    </div>
+      }
+
+
+      </div>
   )
-}
+
+    }
+       
 
 export default Product
